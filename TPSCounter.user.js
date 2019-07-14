5bc1c206a7a95cdde34fbdb032dfa2a83d62cd7f
reviiii <43205544+reviiii@users.noreply.github.com>
2019-07-14 19:17:01 +0200

    Update TPSCounter.user.js

--- TPSCounter.user.js
+++ TPSCounter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TPSCounter
 // @namespace    https://github.com/reviiii/scripts
-// @version      1.9
+// @version      2.0
 // @description  Voegt een TPSCounter toe aan de dynmap van villagercraft
 // @author       Reviiii
 // @match        http://mc.villagercraft.nl:8050/*
@@ -43,9 +43,8 @@ window.TPSCounter = {
     newCounter: 0,
     day: 0,
     period: 60000,
+    precision: 100,
     update: function(time, d) {
-        // var d = new Date();
-        // d = d.getTime();
         this.dateArr[this.newCounter] = d;
         time += this.day*24000;
         if (time<this.timeArr[this.newCounter-1]) {
@@ -62,7 +61,7 @@ window.TPSCounter = {
             this.oldCounter++;
             this.oldCounter %= this.arrSize;
         }
-        var tps = Math.round(((this.timeArr[this.newCounter]-this.timeArr[this.oldCounter])/(this.dateArr[this.newCounter]-this.dateArr[this.oldCounter]))*100000)/100;
+        var tps = Math.round(((this.timeArr[this.newCounter]-this.timeArr[this.oldCounter])/(this.dateArr[this.newCounter]-this.dateArr[this.oldCounter]))*1000*this.precision)/this.precision;
         if (!Number.isNaN(tps)) {
             document.getElementById("tps").innerHTML = tps.toString();
             // document.getElementById("tps").title = ((this.dateArr[this.newCounter]-this.dateArr[this.oldCounter])/1000).toString()+"s";
@@ -71,7 +70,46 @@ window.TPSCounter = {
         }
         this.newCounter++;
         this.newCounter %= this.arrSize;
+    },
+    setOption: function(option, value) {
+        if (Number.isNaN(value)||typeof value!=="number") {
+            throw new Error("Value is Not a Number");
+        }
+        switch (option) {
+            case "period":
+                if (value<0) {
+                    throw new Error("Time travel isn't possible");
+                    return;
+                }
+                this.period = value;
+                var d = this.dateArr[(this.newCounter-1+this.arrSize)%this.arrSize]-value;
+                if (d>0) {
+                    while (this.dateArr[this.oldCounter]>d) { // change oldCounter to speed up period change
+                        this.oldCounter += this.arrSize-1;
+                        this.oldCounter %= this.arrSize;
+                    }
+                    this.oldCounter += 1; // correct oldCounter, as it is now 1 too low
+                    this.oldCounter %= this.arrSize;
+                } else if (this.dateArr[this.newCounter]===0) {
+                    this.oldCounter = 0;
+                } else {
+                    this.oldCounter = this.newCounter
+                }
+                return "period set to "+value.toString()+" ms and oldCounter to "+this.oldCounter;
+                break;
+            case "precision":
+                this.precision = value;
+                return "precision set to "+value.toString()+" ("+Math.log10(value).toString()+" digits)";
+                break;
+        }
+    },
+    toggleGUI: function() { // currently no GUI element is present
+        if (document.getElementById("TPSCounter_GUI").style.visibility==="visible") {
+            document.getElementById("TPSCounter_GUI").style.visibility = "hidden";
+        } else {
+            document.getElementById("TPSCounter_GUI").style.visibility = "visible";
+        }
     }
-}
-}
+} // end of object
+} // end of function start
 })()
