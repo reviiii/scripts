44f557b0cea884685ad74c6f07819cd3c906fc78
reviiii <43205544+reviiii@users.noreply.github.com>
2020-02-11 18:06:39 +0100

    Update TPSCounter.user.js

--- TPSCounter.user.js
+++ TPSCounter.user.js
@@ -7,7 +7,6 @@
 // @match        https://map.villagercraft.nl/*
 // @grant        none
 // ==/UserScript==
-// currently broken, do not update
 
 // als je de dynmap wilt overclocken kan je dynmap.options.updaterate veranderen
 (function() {
