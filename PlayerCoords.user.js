349105faea4075c8db62ca9cd5d252c63a807a7c
reviiii <43205544+reviiii@users.noreply.github.com>
2020-07-08 13:52:01 +0200

    Update PlayerCoords.user.js

--- PlayerCoords.user.js
+++ PlayerCoords.user.js
@@ -15,10 +15,10 @@
     function start() {
         $(dynmap).unbind('worldupdating', start)
         function main(event, player) {
-	    if (player.namefield) {
-	        player.namefield.html(player.name+"<br>"+player.location.x+","+player.location.y+","+player.location.z) // this isn't the best solution as it causes a lot of DOM changes to be made
+            if (player.namefield) {
+                player.namefield.html(player.name+"<br>"+player.location.x+","+player.location.y+","+player.location.z) // this isn't the best solution as it causes a lot of DOM changes to be made
             }
-	}
+        }
         $(dynmap).bind('playeradded', main);
         $(dynmap).bind('playerupdated', main);
         var css = ".dynmap .playerNameNoHealth { top: -16px }"
