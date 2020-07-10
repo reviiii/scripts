d0b14cd668813d653f2dededdc05bb4db731457b
reviiii <43205544+reviiii@users.noreply.github.com>
2020-07-10 15:30:02 +0200

    Update SlimeChunks.user.js

--- SlimeChunks.user.js
+++ SlimeChunks.user.js
@@ -10,7 +10,7 @@
 
 (function() {
     'use strict';
-    var markerURL = "http://localhost"
+    var markerURL = "http://localhost/"
     var $=window.$, dynmap=window.dynmap;
     $(dynmap).bind('worldupdating', start)
     function start() {
