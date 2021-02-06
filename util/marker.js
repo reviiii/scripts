64ebb7abc859c5ce9bd7781db6fb8dc511de278e
reviiii <43205544+reviiii@users.noreply.github.com>
2021-02-06 22:16:46 +0100

    Update marker.js

--- util/marker.js
+++ util/marker.js
@@ -1,7 +1,8 @@
 function createMarkerset({hide=true, id, label, layerprio=0, maxzoom=-1, minzoom=-1, showlabels=false, timestamp=Date.now()}) {
     $(dynmap).trigger("component.markers", {msg:"setupdated", hide, id, label, layerprio, maxzoom, minzoom, showlabels, timestamp});
-    var o = windopw.dynmapmarkersets[id];
+    var o = window.dynmapmarkersets[id];
     o.lines={};o.circles={};o.areas={};o.hide=hide;
+    return o;
 }
 
 function createMarker({x, y=68, z, id, label, icon, iconBase=dynmap.options.url.markers, set="markers", markup=false, desc=null, dim="16x16", minzoom=-1, maxzoom=-1, timestamp=Date.now()}) {
@@ -9,4 +10,5 @@ function createMarker({x, y=68, z, id, label, icon, iconBase=dynmap.options.url.
     dynmap.options.url.markers = iconBase;
     $(dynmap).trigger("component.markers", {msg:"markerupdated", x, y, z, id, label, icon, set, markup, desc, dim, minzoom, maxzoom, ctype:"markers", type:"components", timestamp});
     dynmap.options.url.markers = oldBase;
+    return window.dynmapmarkersets[set][id];
 }
