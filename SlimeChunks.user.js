bd77c82dd85098d591d1400217d848e148d3f5da
reviiii <43205544+reviiii@users.noreply.github.com>
2021-10-08 22:19:40 +0200

    Update SlimeChunks.user.js

--- SlimeChunks.user.js
+++ SlimeChunks.user.js
@@ -1,18 +1,19 @@
 // ==UserScript==
 // @name         Slime Chunks
 // @namespace    https://github.com/reviiii/scripts
-// @version      0.2.1
+// @version      0.2.2
 // @description  Adds slimechunk layer to the dynmap
 // @author       Reviiii
 // @match        https://map.villagercraft.nl/*
 // @grant        none
 // ==/UserScript==
 
-// note: je moet op localhost een server hebben die een 1001x1001 chunks plaatje van de slimechunks geeft op /_markers_/slimechunks.png
+// note: je moet op localhost een server hebben die een 4001x4001 chunks plaatje van de slimechunks geeft op /_markers_/slimechunks.png
 
 (function() {
     'use strict';
     var markerURL = "http://localhost/"
+    var imageSize = 4001;
     var $=window.$, dynmap=window.dynmap;
     $(dynmap).bind('worldupdating', start)
     function start() {
@@ -24,9 +25,9 @@
 
         // make sure all bigMarkers are the right size
         function updateBigMarkerStyle() {
-            var size = 1001*2/dynmap.map.getZoomScale()
+            var size = imageSize*dynmap.map.options.crs.scale(dynmap.map.getZoom())
             bigMarkerStyle.innerHTML = `.dynmap .mapMarker .markerIconBig {
-                /*transform: translate(-50%, -50%);*/
+                transform: translate(50%, 50%);
                 width: ${size}px;
                 height: ${size}px;
                 pointer-events: none;
@@ -37,6 +38,12 @@
                 left: ${size/2+2}px;
             }`
         }
+        $(window.map).on("zoomanim", function() { bigMarkerStyle.innerHTML = `.dynmap .mapMarker .markerIconBig {
+            display: none;
+        }
+        .dynmap .mapMarker .markerNameBig {
+            display: none;
+        }`});
         $(dynmap).bind('zoomchanged', updateBigMarkerStyle)
 
         // add the bigMarker markerset
@@ -45,11 +52,19 @@
         let o = window.dynmapmarkersets.bigMarkers
         o.lines={};o.circles={};o.areas={};o.hide=true;
 
+        // overwrite concatURL function
+        var oldConcatURL = window.concatURL
+        window.concatURL = function(base, addition) {
+            var magic = "overwrite:\n"
+            if (addition.split(magic).length>1) {
+                return addition.split(magic)[1]
+            }
+            return oldConcatURL.call(window, ...arguments)
+        }
+
         // add the slimechunksmarker
-        let oldMarkerURL = dynmap.options.url.markers
-        dynmap.options.url.markers = markerURL
-        $(dynmap).trigger("component.markers", {"msg":"markerupdated","x":-8000,"y":68,"z":-8000,"id":"slimeChunks","label":"","icon":"slimechunks","set":"bigMarkers","markup":false,"desc":null,"dim":"Big","minzoom":-1,"maxzoom":-1,"ctype":"markers","type":"component","timestamp":Date.now()})
-        dynmap.options.url.markers=oldMarkerURL
+        let startCoords = -(imageSize-1)/2*16
+        $(dynmap).trigger("component.markers", {"msg":"markerupdated","x":startCoords+1.5,"y":68,"z":startCoords+0.5,"id":"slimeChunks","label":"","icon":"overwrite:\n"+markerURL+"_markers_/slimechunks","set":"bigMarkers","markup":false,"desc":null,"dim":"Big","minzoom":-1,"maxzoom":-1,"ctype":"markers","type":"component","timestamp":Date.now()})
 
         // make sure all bigMarkers arent clickable
         var bigMarkerElements = document.getElementsByClassName("markerIconBig")
@@ -61,7 +76,7 @@
         // uncheck the " Slime Chunks" checkbox in the layer control, because the hide: true doesn't work for some reason
         let controls = document.getElementsByClassName("leaflet-control-layers-overlays")[0].children
         for (let i=0;i<controls.length;i++) {
-            if (controls[i].children[1].innerHTML===" Slime Chunks"&&controls[i].children[0].checked) controls[i].children[0].click()
+            if (controls[i].children[0].children[1].innerHTML===" Slime Chunks"&&controls[i].children[0].children[0].checked) controls[i].children[0].children[0].click()
         }
     }
 })();
